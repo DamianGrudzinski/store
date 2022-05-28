2831c95d9bcee873b6820c2ec12332ecc0a638d2
DamianGrudzinski <dgrudzinski2@gmail.com>
2022-05-28 11:16:29 +0200

    Add class Category, Club, Product

--- Products/models.py
+++ Products/models.py
@@ -2,3 +2,31 @@ from django.db import models
 
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, null=True, blank=False, on_delete=models.SET_NULL)
+    club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- Products/views.py
+++ Products/views.py
@@ -1,2 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
 
 # Create your views here.
+
+def index(request):
+    available_products = Product.objects.all()
+    return HttpResponse(available_products)
